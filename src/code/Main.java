2486810f5f292d797f5814af250a215a559b7f5d
Milan Bojovic <milanbojovic@gmail.com>
2015-11-17 23:31:32 +0100

    Added Serpinski Triangle set, Removed nonsense Resolution class.

--- src/code/Main.java
+++ src/code/Main.java
@@ -8,15 +8,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    public final int SCREEN_WIDTH   = 1024;
+    public final int SCREEN_HEIGHT  = 768;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Fractal Drawer");
-        Resolution.width = 1024;
-        Resolution.height = 768;
 
         Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-        Scene scene = new Scene(root, Resolution.width, Resolution.height);
+        Scene scene = new Scene(root, SCREEN_WIDTH, SCREEN_HEIGHT);
 
         primaryStage.setScene(scene);
         primaryStage.show();
